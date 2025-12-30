d01c5274d0387a9c5d4c18c6f954adb743c2b01e
deepCfish <deepcfish.camellia@gmail.com>
2025-12-30 21:15:31 +0800

    v1.004

--- internal/backup/gui.go
+++ internal/backup/gui.go
@@ -8,38 +8,16 @@ import (
 )
 
 func opengui() {
-    a := app.New()
-    w := a.NewWindow("Karin")
+	a := app.New()
+	w := a.NewWindow("Hello")
 
-    // Label
-    label := widget.NewLabel("Hello, 小佩可茜忒")
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
 
-    // Button 打开文件对话框
-    openButton := widget.NewButton("选择文件", func() {
-        fd := dialog.NewFileOpen(
-            func(reader fyne.URIReadCloser, err error) {
-                if err != nil {
-                    dialog.ShowError(err, w)
-                    return
-                }
-                if reader == nil {
-                    // 用户取消
-                    return
-                }
-                path := reader.URI().Path()
-                label.SetText("选择的文件: " + path)
-
-                // TODO: 在这里调用你的打包软件逻辑
-                fmt.Println("用户选择了文件:", path)
-            }, w)
-        fd.SetFilter(storage.NewExtensionFileFilter([]string{".zip", ".txt"})) // 可选文件类型
-        fd.Show()
-    })
-
-    // 布局：垂直排列 Label + Button
-    content := container.NewVBox(label, openButton)
-    w.SetContent(container.NewCenter(content))
-
-    w.Resize(fyne.NewSize(800, 600))
-    w.ShowAndRun()
+	w.ShowAndRun()
 }
